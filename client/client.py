e4356263309770d0f97d483d7781bde33f5ea9e4
JingyiMeng6 <60023395+JingyiMeng6@users.noreply.github.com>
2023-07-20 10:29:58 -0700

    setup client interface

--- client/client.py
+++ client/client.py
@@ -1,1 +1,37 @@
 import sys
+import client_lib
+from datetime import datetime
+from time import gmtime, strftime
+
+
+def main():
+    print("\n")
+    client_lib.instructions()
+    client_id = strftime("%Y%m%d%H%M%S", gmtime())
+    file_version_map = {}
+
+    while True:
+
+        client_input = sys.stdin.readline()
+
+        if "<write>" in client_input:
+            print("Entering <write> mode...\n")
+            print("Exiting <write> mode...\n")
+
+        if "<read>" in client_input:
+            print("Entering <read> mode...\n")
+            print("Exiting <read> mode...\n")
+
+        if "<list>" in client_input:
+            print("List all files\n")
+
+        if "<instructions>" in client_input:
+            client_lib.instructions()
+
+        if "<quit>" in client_input:
+            print("Exiting application...")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ client/client_lib.py
@@ -0,0 +1,10 @@
+def instructions():
+    # instructions to the user
+    print ("------------------- INSTRUCTIONS ----------------------")
+    print ("<write> [filename] - write file mode")
+    print ("<end> - finish writing")
+    print ("<read> [filename] - read file mode")
+    print ("<list> - lists all existing files")
+    print ("<instructions> - check the instructions again")
+    print ("<quit> - exits the application")
+    print ("-------------------------------------------------------\n")
